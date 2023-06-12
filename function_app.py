c2ad5a0145e67f3dec6799de734972dc112858de
Sam Gribble <samuel.gribble@yahoo.com>
2023-06-12 12:49:16 -0400

    Added a nice part to the hello statement

--- function_app.py
+++ function_app.py
@@ -17,7 +17,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. This is a result of a different branch :)")
     else:
         return func.HttpResponse(
             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
